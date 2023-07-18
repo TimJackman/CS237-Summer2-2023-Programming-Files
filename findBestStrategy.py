c623805db4d1323d62193f28cde4010e25e43b3d
TimJackman <54875449+TimJackman@users.noreply.github.com>
2023-07-18 12:50:11 -0400

    Add files via upload

---
+++ findBestStrategy.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'findBestStrategy', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ simulateStrategy.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'simulateStrategy', 'points': 1.5, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
