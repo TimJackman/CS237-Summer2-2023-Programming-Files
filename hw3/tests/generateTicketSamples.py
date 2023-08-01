1d74db72e7f4cf58d438f850828a65550bb3bb9e
TimJackman <54875449+TimJackman@users.noreply.github.com>
2023-08-01 15:09:13 -0400

    Updated tests

--- hw3/tests/generateTicketSamples.py
+++ hw3/tests/generateTicketSamples.py
@@ -5,8 +5,7 @@ test = {   'name': 'generateTicketSamples',
     'suites': [   {   'cases': [   {   'code': '>>> sample = generateTicketSamples(100,10)\n'
                                                '>>> \n'
                                                '>>> assert len(sample) == 100\n'
-                                               '>>> assert (sample <= 10).all()\n'
-                                               '>>> assert (sample).all()\n'
+                                               '>>> assert max(sample) < 10\n'
                                                '>>> assert all(sample[i] <= sample[i+1] for i in range(len(sample) - 1))\n',
                                        'hidden': False,
                                        'locked': False}],
