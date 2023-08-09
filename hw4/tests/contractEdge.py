891b455ed45a0abca82d2294cf3145e86b159a1e
TimJackman <54875449+TimJackman@users.noreply.github.com>
2023-08-09 12:37:57 -0400

    Update contractEdge.py

--- hw4/tests/contractEdge.py
+++ hw4/tests/contractEdge.py
@@ -5,7 +5,7 @@ test = {   'name': 'contractEdge',
     'suites': [   {   'cases': [   {   'code': '>>> G = nx.relabel_nodes(nx.MultiGraph(nx.complete_graph(3)), lambda x: colToLetter(x))\n'
                                                ">>> contractEdge(G, ('B','C'))\n"
                                                '>>> assert list(G.edges) == [(\'A\', \'B,C\', 0), (\'A\', \'B,C\', 1)], f"The graph G still has edges: {list(G.edges)} and not {[(\'A\', \'B,C\', 0), '
-                                               '(\'A\', \'B,D\', 1)]} when we tried contracting (B,C) in the complete graph of three vertices" \n',
+                                               '(\'A\', \'B,C\', 1)]} when we tried contracting (B,C) in the complete graph of three vertices" \n',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
